4866434eae3cc623bd4c05bbb0cae0e8e76d40d1
pirozhenko Alexandr <pirog_13@ukr.net>
2016-08-15 09:32:44 +0300

    change icon calendar

--- js/main.js
+++ js/main.js
@@ -19,12 +19,31 @@ values.forEach(function (value, i, values) {
 
 elm.remove();
 
+function range2() {
+	var rng2 = document.querySelector('#range2'),
+			value1 = values[0],
+			value2 = values[1],
+			// thumb1 = document.querySelector('');
+			valColor = document.querySelector('input-value').elementFromPoint(x,y);
+		
+
+ 		console.log (value1);
+ 		console.log (valColor);
+ 		console.log (value2);
+}
+
 
 function range3() {
     var rng3 = document.querySelector('#range3'),
         // value = document.querySelector('.range1-value');
         value = rng3.value;
+        rng3.style.backgroundPosition
 
     value.innerHTML = rng3.value;
-    console.log(value)
+    console.log(value);
+    console.log();
+}
+
+function what() {
+	console.log(this);
 }
